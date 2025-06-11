d5fa2f30a51895b2518080caa8ff70e4ecdcd4f3
41123381801 <41123381801@INF0207>
2025-06-11 13:41:13 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoPerfilBeatrizG.Domain/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace ProjetoPerfilBeatrizG.Domain
+{
+    public class Class1
+    {
+
+    }
+
+    public class Cantor
+    {
+       public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Musica { get; set; }
+    }
+}
